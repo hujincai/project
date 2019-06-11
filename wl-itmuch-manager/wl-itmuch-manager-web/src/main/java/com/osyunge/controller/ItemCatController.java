fb0509f78cdd71b7e162c29230b7dcb004cfaf54
hujincai <h18401632295@163.com>
2019-06-11 14:15:11 +0800

    删除多余的Import包

--- wl-itmuch-manager/wl-itmuch-manager-web/src/main/java/com/osyunge/controller/ItemCatController.java
+++ wl-itmuch-manager/wl-itmuch-manager-web/src/main/java/com/osyunge/controller/ItemCatController.java
@@ -4,7 +4,6 @@ import com.osyunge.dataobject.EasyUITreeNode;
 import com.osyunge.inter.ItemCatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
