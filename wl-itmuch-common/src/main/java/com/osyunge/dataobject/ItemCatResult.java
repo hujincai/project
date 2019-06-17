73863a51a069b7b69ce0d46af14ff47e17f2b6bf
hujincai <h18401632295@163.com>
2019-06-17 18:29:39 +0800

    common 新增文件Result

---
+++ wl-itmuch-common/src/main/java/com/osyunge/dataobject/ItemCatResult.java
@@ -0,0 +1,14 @@
+package com.osyunge.dataobject;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by 胡金才
+ * 2019/6/14 9:37
+ */
+@Data
+public class ItemCatResult {
+    private List data;
+}
